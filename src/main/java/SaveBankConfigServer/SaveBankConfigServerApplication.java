0168cbb8b54d90926a6cac166940382adf42c3d8
Lwazi <lwazigxogxa@gmail.com>
2023-04-18 08:51:49 +0200

    first commit

---
+++ src/main/java/SaveBankConfigServer/SaveBankConfigServerApplication.java
@@ -0,0 +1,13 @@
+package SaveBankConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveBankConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveBankConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SaveBankConfigServer/SaveBankConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package SaveBankConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveBankConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
